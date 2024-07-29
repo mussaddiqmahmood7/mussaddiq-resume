4d20f48872eb4f4a4e24cc6880346b9720329c92
mussaddiqmahmood7 <mussaddiqmahmood7@gmail.com>
2024-07-29 16:41:56 +0500

    links are added , stars number are reduced

--- src/app/_components/Stars.tsx
+++ src/app/_components/Stars.tsx
@@ -1,14 +1,25 @@
-'use client'
-import React, { useEffect } from 'react'
-function generateStarsCSS() {
-    let styles = '';
-  
-    for (let i = 0; i < 200; i++) {
-      const top = Math.random() * 100;
-      const left = Math.random() * 99;
-      const size = Math.random() * 3 + 2;
-      const duration = Math.random() * 5 + 2;
-      styles += `
+"use client";
+import React, { useEffect, useState } from "react";
+
+function generateStarsCSS(starCount: number) {
+  let styles = "";
+
+  for (let i = 0; i < starCount; i++) {
+    const top = Math.random() * 100;
+    let left;
+    if (window.innerWidth <= 768) {
+      left = Math.random() < 0.5
+        ? Math.floor(Math.random() * 5) + 1
+        : Math.floor(Math.random() * 5) + 94;
+    } else {
+      left = Math.random() < 0.5
+        ? Math.floor(Math.random() * 25)
+        : Math.ceil(Math.random() * 24) + 75;
+    }
+    console.log(left);
+    const size = Math.random() * 3 + 2;
+    const duration = Math.random() * 5 + 2;
+    styles += `
         .star-${i} {
           top: ${top}%;
           left: ${left}%;
@@ -17,24 +28,34 @@ function generateStarsCSS() {
           animation-duration: ${duration}s;
         }
       `;
-    }
-  
-    return styles;
   }
-function Stars() {
-    useEffect(() => {
-        const styleSheet = document.createElement("style");
-        styleSheet.type = "text/css";
-        styleSheet.innerText = generateStarsCSS();
-        document.head.appendChild(styleSheet);
-      }, []);
-  return (
-    <div className='hidden dark:flex'>
-      {Array.from({ length: 200 }).map((_, index) => (
-          <div key={index} className={`star-${index} star absolute transition-all `}></div>
-        ))}
-    </div>
-  )
+
+  return styles;
 }
 
-export default Stars
+function Stars() {
+  const [starCount, setStarCount] = useState(50);
+
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      setStarCount(25);
+    }
+    const styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = generateStarsCSS(starCount);
+    document.head.appendChild(styleSheet);
+  }, [starCount]);
+
+  return (
+    <div className="hidden dark:flex">
+      {Array.from({ length: starCount }).map((_, index) => (
+        <div
+          key={index}
+          className={`star-${index} star absolute transition-all `}
+        ></div>
+      ))}
+    </div>
+  );
+}
+
+export default Stars;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Mods from "./_components/Mods";
 import AnimatedDiv from "./_components/AnimateDiv";
 import Linkedin from "./_components/Linkden";
 import Github from "./_components/Github";
+import Link from "next/link";
 function H2({ title }: { title: string }) {
   return (
     <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold dark:text-white">
@@ -48,7 +49,7 @@ const skills = [
   "Typescript",
   "Javascript",
   "Git",
-  "Payload CMS", 
+  "Payload CMS",
 ];
 
 const workExperience = [
@@ -88,7 +89,7 @@ export default function Home() {
     <main className="max-w-screen-lg mx-auto py-16 px-4 lg:px-8 text-black dark:text-white  ">
       <Mods />
       <div className="w-full relative overflow-hidden rounded-xl border flex flex-col gap-10 py-5 px-3 sm:px-5 lg:px-10 lg:py-12 bg-white dark:bg-gray-800 shadow-md dark:shadow-lg">
-      <AnimatedDiv/>
+        <AnimatedDiv />
         <div className="flex flex-col gap-2 text-center lg:text-start z-10">
           <h1 className="text-2xl md:text-4xl lg:text-5xl p-2 bg-gray-100 dark:bg-gray-700 font-bold w-full mx-auto tracking-wide">
             Mussaddiq Mahmood
@@ -101,69 +102,70 @@ export default function Home() {
           <H2 title="SUMMARY" />
           <H3Justify title="Results-driven Software Engineer specializing in full-stack development with expertise in Next.js, React, Preact, and Astro. Proven track record of delivering robust web applications with a focus on front-end and back-end technologies. Experienced in working with TypeScript, JavaScript, and modern web frameworks. Eager to contribute to dynamic development environments." />
         </div>
-            <div className="flex gap-10 justify-around w-full items-center mx-auto py-2 border-y-[2px] border-black dark:border-white">
-              <a href="tel:03067663989" className="flex items-center gap-3">
-                <div className="hidden dark:flex">
-                  <Phone dark={true} />
-                </div>
-                <div className="flex dark:hidden">
-                  <Phone dark={false} />
-                </div>
-              </a>
-              <a
-                href="mailto:mussaddiqmahmood7@gmail.com"
-                className="flex items-center gap-3"
-              >
-                <div className="hidden dark:flex">
-                  <Email dark={true} />
-                </div>
-                <div className="flex dark:hidden">
-                  <Email dark={false} />
-                </div>
-              </a>
-              <a
-                href="https://github.com/mussaddiqmahmood7"
-                className="flex items-center gap-3"
-              >
-                <div className="hidden dark:flex">
-                  <Github dark={true} />
-                </div>
-                <div className="flex dark:hidden">
-                  <Github dark={false} />
-                </div>
-              </a>
-              <a
-                href="https://www.linkedin.com/in/mussaddiq-mahmood"
-                className="flex items-center gap-3"
-              >
-                <div className="hidden dark:flex">
-                  <Linkedin dark={true} />
-                </div>
-                <div className="flex dark:hidden">
-                  <Linkedin dark={false} />
-                </div>
-              </a>
-             
+        <div className="flex gap-10 justify-around w-full items-center mx-auto py-2 border-y-[2px] border-black dark:border-white">
+          <a href="tel:03067663989" className="flex items-center gap-3">
+            <div className="hidden dark:flex">
+              <Phone dark={true} />
             </div>
-         
+            <div className="flex dark:hidden">
+              <Phone dark={false} />
+            </div>
+          </a>
+          <a
+            href="mailto:mussaddiqmahmood7@gmail.com"
+            className="flex items-center gap-3"
+          >
+            <div className="hidden dark:flex">
+              <Email dark={true} />
+            </div>
+            <div className="flex dark:hidden">
+              <Email dark={false} />
+            </div>
+          </a>
+          <a
+            href="https://github.com/mussaddiqmahmood7"
+            className="flex items-center gap-3"
+          >
+            <div className="hidden dark:flex">
+              <Github dark={true} />
+            </div>
+            <div className="flex dark:hidden">
+              <Github dark={false} />
+            </div>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/mussaddiq-mahmood"
+            className="flex items-center gap-3"
+          >
+            <div className="hidden dark:flex">
+              <Linkedin dark={true} />
+            </div>
+            <div className="flex dark:hidden">
+              <Linkedin dark={false} />
+            </div>
+          </a>
+        </div>
 
-          <div className="w-full flex flex-col gap-7">
-            <H2 title="EDUCATION" />
-            <div className="grid grid-cols-2 gap-5">
-              <div className="flex flex-col">
-                <H3Bold title="Bachelor of Science in Computer Science" />
+        <div className="w-full flex flex-col gap-7">
+          <H2 title="EDUCATION" />
+          <div className="grid grid-cols-2 gap-5">
+            <div className="flex flex-col">
+              <H3Bold title="Bachelor of Science in Computer Science" />
+              <Link href="https://uo.edu.pk/" target="_blank" className="hover:underline">
                 <H3 title="University of Okara" />
-                <H3 title="2020 - 2024" />
-              </div>
-              <div className="flex flex-col">
-                <H3Bold title="Intermediate in Computer Science" />
+              </Link>
+              <H3 title="2020 - 2024" />
+            </div>
+            <div className="flex flex-col">
+              <H3Bold title="Intermediate in Computer Science" />
+              <Link href="https://pgc.edu/" target="_blank" className="hover:underline">
                 <H3 title="Punjab Group Of Colleges Chishtian" />
-                <H3 title="2018 - 2020" />
-              </div>
+              </Link>
+              <H3 title="2018 - 2020" />
             </div>
           </div>
-          
-        
+        </div>
+
         <div className="w-full h-[2px] rounded-full bg-black dark:bg-white z-10"></div>
         <div className="w-full flex flex-col gap-7 z-10">
           <H2 title="SKILLS" />
@@ -201,7 +203,7 @@ export default function Home() {
         <div className="w-full flex flex-col gap-7 z-10">
           <H2 title="PROJECTS" />
           <p className="text-sm text-gray-600 dark:text-gray-400">
-            Note: The following projects are just my personal projects not company.
+            Note: The following projects are just my personal projects.
           </p>
           <div className="flex flex-col gap-5">
             {projects.map((project, index) => (
